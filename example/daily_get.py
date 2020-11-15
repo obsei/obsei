919bde58b4c5298afa3961a664aa93d678a00f37
lalitpagaria <pagaria.lalit@gmail.com>
2020-11-15 15:54:19 +0100

    Adding daily get as a sink

--- example/daily_get.py
+++ example/daily_get.py
@@ -2,105 +2,19 @@ import logging
 import os
 import sys
 from pathlib import Path
-from typing import Any, Dict, Optional
 
-from datetime import timezone
-from dateutil import parser
-import pytz
-
-from obsei.sink.base_sink import Convertor
-from obsei.sink.http_sink import HttpSink, HttpSinkConfig
+from obsei.sink.dailyget_sink import DailyGetSink, DailyGetSinkConfig
 from obsei.source.twitter_source import TwitterSource, TwitterSourceConfig
-from obsei.text_analyzer import AnalyzerResponse, TextAnalyzer
-from obsei.utils import flatten_dict
+from obsei.text_analyzer import TextAnalyzer
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-TWITTER_URL_PREFIX = "https://twitter.com/"
-IST_TZ = pytz.timezone('Asia/Kolkata')
-
-
-class PayloadConvertor(Convertor):
-    def convert(self, analyzer_response: AnalyzerResponse, base_payload: Optional[Dict[str, Any]] = None, **kwargs) -> Dict[str, Any]:
-        request_payload = base_payload or {}
-
-        user_url = ""
-        positive = 0.0
-        negative = 0.0
-        text = ""
-        tweet_id = None
-        created_at_str = None
-        classification_list = []
-
-        flat_dict = flatten_dict(analyzer_response.to_dict())
-        for k, v in flat_dict.items():
-            if "username" in k:
-                user_url = TWITTER_URL_PREFIX + v
-            elif "text" in k:
-                text = str(v).replace("\n", " ")
-            elif "positive" in k:
-                positive = float(v)
-            elif "negative" in k:
-                negative = float(v)
-            elif "meta_id" in k:
-                tweet_id = v
-            elif "created_at" in k:
-                created_at_str = v
-            elif "classification" in k and len(classification_list) < 2:
-                classification_list.append(k.rsplit("_", 1)[1])
-
-        if created_at_str:
-            created_at = parser.isoparse(created_at_str)
-            created_at_str = created_at.replace(
-                tzinfo=timezone.utc
-            ).astimezone(tz=IST_TZ).strftime('%Y-%m-%d %H:%M:%S')
-
-        tweet_url = user_url + "/status/" + tweet_id
-        # Sentiment rules
-        if negative > 8.0:
-            sentiment = "Strong Negative"
-        elif 0.3 < negative <= 8.0:
-            sentiment = "Negative"
-        elif positive >= 0.8:
-            sentiment = "Strong Positive"
-        elif 0.4 < positive < 0.8:
-            sentiment = "Positive"
-        else:
-            sentiment = "Neutral"
-
-        enquiry = {
-            "Source": analyzer_response.source_name + " " + os.environ['DAILYGET_QUERY'],
-            "FeedbackBy": user_url,
-            "Sentiment": sentiment,
-            "TweetUrl": tweet_url,
-            "FormattedText": text,
-            "PredictedCategories": ",".join(classification_list),
-        }
-
-        if created_at_str:
-            enquiry["ReportedAt"] = created_at_str
-
-        kv_str_list = [k + ": " + str(v) for k, v in enquiry.items()]
-        request_payload["enquiryMessage"] = "\n".join(kv_str_list)
-        return request_payload
-
-
-sink_config = HttpSinkConfig(
-    convertor=PayloadConvertor(),
+sink_config = DailyGetSinkConfig(
     url=os.environ['DAILYGET_URL'],
-    base_payload={
-        "partnerId": os.environ['DAILYGET_PARTNER_ID'],
-        "consumerPhoneNumber": os.environ['DAILYGET_CONSUMER_NUMBER'],
-    },
-    payload_mapping={
-      "processed_text": ["enquiryMessage"],
-      "classification": ["enquiryMessage"],
-      "meta": ["enquiryMessage"],
-    },
-    field_conversion={
-        "enquiryMessage": "flat_string"
-    }
+    partner_id=os.environ['DAILYGET_PARTNER_ID'],
+    consumer_phone_number=os.environ['DAILYGET_CONSUMER_NUMBER'],
+    source_information="Twitter " + os.environ['DAILYGET_QUERY']
 )
 
 dir_path = Path(__file__).resolve().parent.parent
@@ -116,7 +30,7 @@ source_config = TwitterSourceConfig(
 )
 
 source = TwitterSource()
-sink = HttpSink()
+sink = DailyGetSink()
 text_analyzer = TextAnalyzer(
     classifier_model_name="joeddav/bart-large-mnli-yahoo-answers",
  #   classifier_model_name="joeddav/xlm-roberta-large-xnli",
---
+++ obsei/sink/dailyget_sink.py
@@ -0,0 +1,144 @@
+import logging
+from copy import deepcopy
+from datetime import timezone
+from typing import Any, Dict, List, Optional
+
+import pytz
+import requests
+from dateutil import parser
+
+from obsei.sink.base_sink import Convertor
+from obsei.sink.http_sink import HttpSink, HttpSinkConfig
+from obsei.text_analyzer import AnalyzerResponse
+from obsei.utils import flatten_dict
+
+logger = logging.getLogger(__name__)
+
+
+TWITTER_URL_PREFIX = "https://twitter.com/"
+IST_TZ = pytz.timezone('Asia/Kolkata')
+
+
+class PayloadConvertor(Convertor):
+    def convert(
+        self,
+        analyzer_response: AnalyzerResponse,
+        base_payload: Optional[Dict[str, Any]] = None,
+        **kwargs
+    ) -> Dict[str, Any]:
+        request_payload = base_payload or {}
+
+        if analyzer_response.source_name != "Twitter":
+            return {**request_payload, **analyzer_response.to_dict()}
+
+        source_information = kwargs["source_information"]
+
+        user_url = ""
+        positive = 0.0
+        negative = 0.0
+        text = ""
+        tweet_id = None
+        created_at_str = None
+        classification_list = []
+
+        flat_dict = flatten_dict(analyzer_response.to_dict())
+        for k, v in flat_dict.items():
+            if "username" in k:
+                user_url = TWITTER_URL_PREFIX + v
+            elif "text" in k:
+                text = str(v).replace("\n", " ")
+            elif "positive" in k:
+                positive = float(v)
+            elif "negative" in k:
+                negative = float(v)
+            elif "meta_id" in k:
+                tweet_id = v
+            elif "created_at" in k:
+                created_at_str = v
+            elif "classification" in k and len(classification_list) < 2:
+                classification_list.append(k.rsplit("_", 1)[1])
+
+        if created_at_str:
+            created_at = parser.isoparse(created_at_str)
+            created_at_str = created_at.replace(
+                tzinfo=timezone.utc
+            ).astimezone(tz=IST_TZ).strftime('%Y-%m-%d %H:%M:%S')
+
+        tweet_url = user_url + "/status/" + tweet_id
+        # Sentiment rules
+        if negative > 8.0:
+            sentiment = "Strong Negative"
+        elif 0.3 < negative <= 8.0:
+            sentiment = "Negative"
+        elif positive >= 0.8:
+            sentiment = "Strong Positive"
+        elif 0.4 < positive < 0.8:
+            sentiment = "Positive"
+        else:
+            sentiment = "Neutral"
+
+        enquiry = {
+            "Source": source_information,
+            "FeedbackBy": user_url,
+            "Sentiment": sentiment,
+            "TweetUrl": tweet_url,
+            "FormattedText": text,
+            "PredictedCategories": ",".join(classification_list),
+        }
+
+        if created_at_str:
+            enquiry["ReportedAt"] = created_at_str
+
+        kv_str_list = [k + ": " + str(v) for k, v in enquiry.items()]
+        request_payload["enquiryMessage"] = "\n".join(kv_str_list)
+        return request_payload
+
+
+class DailyGetSinkConfig(HttpSinkConfig):
+    def __init__(
+        self,
+        url: str,
+        partner_id: str,
+        consumer_phone_number: str,
+        source_information: str,
+    ):
+        self.source_information = source_information
+        super(DailyGetSinkConfig, self).__init__(
+            url=url,
+            convertor=PayloadConvertor(),
+            headers={
+                "Content-type": "application/json"
+            },
+            base_payload={
+                "partnerId": partner_id,
+                "consumerPhoneNumber": consumer_phone_number,
+            },
+            payload_mapping=None,
+            field_conversion=None,
+        )
+
+
+class DailyGetSink(HttpSink):
+    def send_data(self, analyzer_responses: List[AnalyzerResponse], config: DailyGetSinkConfig):
+        headers = config.headers
+
+        payloads = []
+        responses = []
+        for analyzer_response in analyzer_responses:
+            payloads.append(config.convertor.convert(
+                analyzer_response=analyzer_response,
+                base_payload=deepcopy(config.base_payload),
+                source_information=config.source_information,
+            ))
+
+        for payload in payloads:
+            response = requests.post(
+                url=config.url,
+                json=payload,
+                headers=headers,
+            )
+
+            logger.info(f"response='{response.__dict__}'")
+            responses.append(response)
+
+        return responses
