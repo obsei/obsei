f660ea2962c74259afca3f399621157a05142c21
lalitpagaria <pagaria.lalit@gmail.com>
2020-12-06 22:42:47 +0100

    Adding interpolation config as sample

--- example/sdk_config_example.py
+++ example/sdk_config_example.py
@@ -14,12 +14,12 @@ obsei_configuration = ObseiConfiguration(
 twitter_source_config = obsei_configuration.get_twitter_source_config()
 http_sink_config = obsei_configuration.get_http_sink_config()
 daily_get_sink_config = obsei_configuration.get_daily_get_sink_config()
+text_analyzer = obsei_configuration.get_text_analyzer()
+analyzer_config = obsei_configuration.get_analyzer_config()
 # docker run -d --name elasticsearch -p 9200:9200 -e "discovery.type=single-node" elasticsearch:7.9.2
 elasticsearch_sink_config = obsei_configuration.get_elasticsearch_sink_config()
 # Start jira server locally `atlas-run-standalone --product jira`
 jira_sink_config = obsei_configuration.get_jira_sink_config()
-text_analyzer = obsei_configuration.get_text_analyzer()
-analyzer_config = obsei_configuration.get_analyzer_config()
 
 
 
