f3ef1ae51baa9913b00b46eaa3f5a547837b8c18
lalitpagaria <pagaria.lalit@gmail.com>
2020-12-29 19:19:14 +0100

    Updating setup.py for next release

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="obsei",
-    version="0.0.3",
+    version="0.0.4dev0",
     author="Lalit Pagaria",
     author_email="pagaria.lalit@gmail.com",
     description="Observe PoI text data from the various sources, segment it and then inform about it",
