1452d2869a1eb04f370fff5e72399b8fcb2e72eb
lalitpagaria <pagaria.lalit@gmail.com>
2021-05-28 03:17:26 +0530

    Adding google news and website crawler as source

---
+++ example/google_news_example.py
@@ -0,0 +1,26 @@
+from obsei.source.google_news_source import GoogleNewsConfig, GoogleNewsSource
+
+# Only fetch title and highlight
+source_config_without_full_text = GoogleNewsConfig(
+    query="bitcoin",
+    max_results=5,
+)
+
+# Fetch full news article
+source_config_with_full_text = GoogleNewsConfig(
+    query="bitcoin",
+    max_results=5,
+    fetch_article=True,
+)
+
+source = GoogleNewsSource()
+
+news_articles_without_full_text = source.lookup(source_config_without_full_text)
+
+news_articles_with_full_text = source.lookup(source_config_with_full_text)
+
+for article in news_articles_without_full_text:
+    print(article.__dict__)
+
+for article in news_articles_with_full_text:
+    print(article.__dict__)
