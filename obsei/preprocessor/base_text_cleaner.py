cf0a6f8b7346da74a5b576891ba7934092a495a7
shahrukhx01 <sk28671@gmail.com>
2021-05-29 11:07:50 +0200

    run black formatter on base cleaner

--- obsei/preprocessor/base_text_cleaner.py
+++ obsei/preprocessor/base_text_cleaner.py
@@ -6,9 +6,11 @@ from pydantic import BaseModel, PrivateAttr
 
 from obsei.analyzer.base_analyzer import AnalyzerRequest
 
+
 class BaseTextCleanerConfig(BaseModel):
     TYPE: str = "Base"
     text_cleaning_functions: Optional[List[Dict]]
+
     class Config:
         arbitrary_types_allowed = True
 
@@ -18,15 +20,13 @@ class BaseTextCleaner(BaseModel):
     stop_words: Optional[List[str]] = []
     domain_keywords: Optional[List[Dict]] = []
     stemmer: Any
+
     def __init__(self, **data: Any):
         super().__init__(**data)
-    
+
     @abstractmethod
     def clean_input(
-        self,
-        input_list: List[AnalyzerRequest],
-        config: BaseTextCleanerConfig,
-        **kwargs
+        self, input_list: List[AnalyzerRequest], config: BaseTextCleanerConfig, **kwargs
     ) -> List[AnalyzerRequest]:
         pass
 
