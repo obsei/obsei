ca1f8ecde28ac6003c6112cffbb690a235e86f0b
Shahrukh Khan <sk28671@gmail.com>
2021-06-02 22:01:56 +0200

    add preprocessor/cleaner (#110)
    * add preprocessor boilerplate
    
    * add text preprocessing baseline functions
    
    * fix formatting and design bugs text cleaner
    
    * update gitignore
    
    * Delete .vscode directory
    
    * remove temp main from text cleaner
    
    * run black formatter on base cleaner
    
    * fix nltk download and rename base classes for preprocessor
    
    * rm temp main
    
    * refactor text cleaning execution
    
    * update requirements
    
    * document code and update config design
    
    * rename preprocess executor
    
    * add unit tests
    
    * nltk stopwords download in test
    
    * nltk stopwords download in test
    
    * Doing some code refactoring based on existing and future design consideration
    
    * Using correct date parse function
    
    * Fixing expected output of datetime cleaning function
    
    Co-authored-by: lalitpagaria <pagaria.lalit@gmail.com>

---
+++ obsei/preprocessor/text_cleaner.py
@@ -0,0 +1,59 @@
+from obsei.analyzer.base_analyzer import AnalyzerRequest
+from obsei.preprocessor.base_preprocessor import (
+    BaseTextPreprocessor,
+    BaseTextProcessorConfig,
+)
+from obsei.preprocessor.text_cleaning_function import *
+from obsei.preprocessor.text_tokenizer import BaseTextTokenizer, NLTKTextTokenizer
+
+
+class TextCleanerConfig(BaseTextProcessorConfig):
+    cleaning_functions: Optional[List[TextCleaningFunction]] = None
+    stop_words_language: Optional[str] = "english"
+    stop_words: Optional[List[str]] = None
+    domain_keywords: Optional[Tuple[str, str]] = None
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+
+        if not self.cleaning_functions:
+            self.cleaning_functions = [
+                ToLowerCase(),
+                RemoveWhiteSpaceAndEmptyToken(),
+                RemovePunctuation(),
+                RemoveSpecialChars(),
+                DecodeUnicode(),
+                RemoveDateTime(),
+                ReplaceDomainKeywords(domain_keywords=self.domain_keywords),
+                TokenStemming(),
+                RemoveStopWords(
+                    language=self.stop_words_language, stop_words=self.stop_words
+                ),
+                RemoveWhiteSpaceAndEmptyToken(),
+            ]
+
+
+class TextCleaner(BaseTextPreprocessor):
+    text_tokenizer: Optional[BaseTextTokenizer] = None
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+
+        if not self.text_tokenizer:
+            self.text_tokenizer = NLTKTextTokenizer()
+
+    def preprocess_input(
+        self,
+        input_list: List[AnalyzerRequest],
+        config: TextCleanerConfig,
+        **kwargs,
+    ) -> List[AnalyzerRequest]:
+        for input_data in input_list:
+            tokens: List[str] = self.text_tokenizer.tokenize_text(
+                input_data.processed_text
+            )
+            for cleaning_function in config.cleaning_functions:
+                tokens = cleaning_function.execute(tokens)
+            input_data.processed_text = " ".join(tokens)
+
+        return input_list
