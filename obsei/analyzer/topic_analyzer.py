a769db70c4736258c6ae43dac4b1f9aa7726e2a8
Akarsh Gajbhiye <akarshgajbhiye@gmail.com>
2021-08-14 22:38:01 +0530

    unfactored base logic

--- obsei/analyzer/topic_analyzer.py
+++ obsei/analyzer/topic_analyzer.py
@@ -14,12 +14,12 @@ import re
 from obsei.payload import TextPayload
 from obsei.postprocessor.inference_aggregator import InferenceAggregatorConfig
 from obsei.postprocessor.inference_aggregator_function import ClassificationAverageScore
-from obsei.analyzer.topic_analyzer_utils import get_topics_by_cluster, get_umap_embedings, cluster_embeddings, get_vec_lda
+from obsei.analyzer.topic_analyzer_utils import get_topics_by_cluster, get_umap_embedings, cluster_embeddings, get_vec_lda, Autoencoder
 from sentence_transformers import SentenceTransformer
 from obsei.preprocessor.text_cleaner import TextCleanerConfig, TextCleaner
 from obsei.preprocessor.text_tokenizer import NLTKTextTokenizer
 from obsei.preprocessor.text_cleaning_function import RemovePunctuation, RemoveStopWords, RemoveWhiteSpaceAndEmptyToken, ToLowerCase, TokenStemming
-
+import numpy as np
 logger = logging.getLogger(__name__)
 
 
@@ -85,10 +85,7 @@ class TopicClassificationAnalyzer(BaseAnalyzer):
         return  analyzer_output
 
     def _get_topic_lda(self, source_input_list, source_name):
-        token_lists =  self._prepare_tokens(source_input_list)
-        dictionary = corpora.Dictionary(token_lists)
-        corpus = [dictionary.doc2bow(text) for text in token_lists]
-        _, lda_model = self._get_lda_embeddings(dictionary,corpus )
+        _, lda_model = self._get_lda_embeddings(source_input_list)
         topics = lda_model.show_topics()
         topics_list = [ re.findall(r'"([^"]*)"', t[1]) for t in  topics]
         analyzer_output = [TextPayload(
@@ -98,9 +95,11 @@ class TopicClassificationAnalyzer(BaseAnalyzer):
         ) for t in topics_list]
         return analyzer_output
 
-    def _get_lda_embeddings(self, dictionary ,corpus, num_topics= 10):
-        ldamodel = LdaModel(corpus, num_topics= num_topics , id2word=dictionary,
-                                                   passes=20)
+    def _get_lda_embeddings(self, source_input_list, num_topics= 10):
+        token_lists = self._prepare_tokens(source_input_list)
+        dictionary = corpora.Dictionary(token_lists)
+        corpus = [dictionary.doc2bow(text) for text in token_lists]
+        ldamodel = LdaModel(corpus, num_topics= num_topics , id2word=dictionary, passes=20)
         embeddings = get_vec_lda(ldamodel, corpus, num_topics )
         return embeddings, ldamodel
 
@@ -114,8 +113,24 @@ class TopicClassificationAnalyzer(BaseAnalyzer):
         tokenized_texts = [tokenizer.tokenize_text(t.processed_text) for t in cleaner_responses]
         return tokenized_texts
 
-    def _get_topic_ldabert(self, source_list):
-        
+    def _get_topic_ldabert(self, source_input_list,source_name, umap_n_neighbors = 10, umap_n_components = 5,min_cluster_size = 10  ):
+        gamma = 15 #modelparameter , importance of lda
+        docs = [
+            source_response.processed_text[: self._max_length]
+            for source_response in source_input_list
+        ]
+        embeddings_bert = self._get_transformer_embeddings(docs)
+        embeddings_lda, _ = self._get_lda_embeddings(source_input_list)
+        embeddings_ldabert = np.c_[embeddings_lda * gamma, embeddings_bert]
+        auto_encoder = Autoencoder()
+        auto_encoder.fit(embeddings_ldabert)
+        ldabert_embeddings = auto_encoder.encoder.predict(embeddings_ldabert)
+        umap_embeddings = get_umap_embedings(embeddings=ldabert_embeddings, n_neighbors=umap_n_neighbors,
+                                             n_component=umap_n_components)
+        clusters = cluster_embeddings(umap_embeddings, min_cluster_size=min_cluster_size)
+        topics_per_cluster = get_topics_by_cluster(docs, clusters, source_name)
+        return topics_per_cluster
+
 
 if __name__ == '__main__':
     from obsei.source.playstore_scrapper import PlayStoreScrapperConfig, PlayStoreScrapperSource
@@ -127,18 +142,25 @@ if __name__ == '__main__':
         # For example - https://play.google.com/store/apps/details?id=com.google.android.gm&hl=en&gl=US
         # `com.google.android.gm` is the package_name for xcode and `us` is country.
         countries=["in"],
-        package_name="tv.twitch.android.app",
-        lookup_period="1h"  # Lookup period from current time, format: `<number><d|h|m>` (day|hour|minute)
+        package_name="com.application.zomato",
+        lookup_period="1h",
+        max_count = 2000 # Lookup period from current time, format: `<number><d|h|m>` (day|hour|minute)
     )
 
     # initialize play store reviews retriever
     source = PlayStoreScrapperSource()
     source_list = source.lookup(source_config)
     print("downld")
-    topic_analyzer = TopicClassificationAnalyzer(model_name_or_path = "paraphrase-distilroberta-base-v2",method = "BERT")
+    topic_analyzer = TopicClassificationAnalyzer(model_name_or_path = "paraphrase-TinyBERT-L6-v2 ",method = "BERT")
     #clusters = topic_analyzer.analyze_input(source_list)
+    x = topic_analyzer._get_topic_ldabert(source_list,"ae")
     y = topic_analyzer._get_topic_lda(source_list, "pc")
     z = topic_analyzer._get_topic_bert(source_list, "cc")
-    x = 1
+
+    top_ldabert = [t.processed_text for t in x]
+    top_bert = [t.processed_text for t in z]
+    top_lda = [t.processed_text for t in y]
+
+    a = 1
 
 
