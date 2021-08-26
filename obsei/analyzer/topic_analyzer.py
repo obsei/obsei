a0fdfcf63519c679dd0fdcdb61176909d26b67ca
Akarsh Gajbhiye <akarshgajbhiye@gmail.com>
2021-08-26 21:59:27 +0530

    refactor

--- obsei/analyzer/topic_analyzer.py
+++ obsei/analyzer/topic_analyzer.py
@@ -5,21 +5,31 @@ from pydantic import Field, PrivateAttr
 from transformers import Pipeline, pipeline
 from gensim import corpora
 from gensim.models.ldamodel import LdaModel
-from obsei.analyzer.base_analyzer import (
-    BaseAnalyzer,
-    BaseAnalyzerConfig,
-    MAX_LENGTH
-)
+from obsei.analyzer.base_analyzer import BaseAnalyzer, BaseAnalyzerConfig, MAX_LENGTH
+from torch import Tensor
 import re
 from obsei.payload import TextPayload
 from obsei.postprocessor.inference_aggregator import InferenceAggregatorConfig
 from obsei.postprocessor.inference_aggregator_function import ClassificationAverageScore
-from obsei.analyzer.topic_analyzer_utils import get_topics_by_cluster, get_umap_embedings, cluster_embeddings, get_vec_lda, Autoencoder
+from obsei.analyzer.topic_analyzer_utils import (
+    get_topics_by_cluster,
+    get_umap_embedings,
+    cluster_embeddings,
+    get_vec_lda,
+    Autoencoder,
+)
 from sentence_transformers import SentenceTransformer
 from obsei.preprocessor.text_cleaner import TextCleanerConfig, TextCleaner
 from obsei.preprocessor.text_tokenizer import NLTKTextTokenizer
-from obsei.preprocessor.text_cleaning_function import RemovePunctuation, RemoveStopWords, RemoveWhiteSpaceAndEmptyToken, ToLowerCase, TokenStemming
+from obsei.preprocessor.text_cleaning_function import (
+    RemovePunctuation,
+    RemoveStopWords,
+    RemoveWhiteSpaceAndEmptyToken,
+    ToLowerCase,
+    TokenStemming,
+)
 import numpy as np
+
 logger = logging.getLogger(__name__)
 
 
@@ -28,23 +38,24 @@ class TopicAnalyzerConfig(BaseAnalyzerConfig):
     labels: List[str]
     multi_class_classification: bool = True
     aggregator_config: InferenceAggregatorConfig = Field(
-        InferenceAggregatorConfig(
-            aggregate_function=ClassificationAverageScore()
-        )
+        InferenceAggregatorConfig(aggregate_function=ClassificationAverageScore())
     )
+
 
 class TopicClassificationAnalyzer(BaseAnalyzer):
     _pipeline: Pipeline = PrivateAttr()
     _max_length: int = PrivateAttr()
     TYPE: str = "TopicModelling"
     model_name_or_path: str
-    method : str
+    method: str
 
     def __init__(self, **data: Any):
         super().__init__(**data)
         self._max_length = MAX_LENGTH
 
-    def _get_topics(self, source_response_list, source_name):
+    def _get_topics(
+        self, source_response_list: List[TextPayload], source_name: str
+    ) -> List[TextPayload]:
         if self.method == "BERT":
 
             return self._get_topic_bert(source_response_list, source_name)
@@ -52,22 +63,35 @@ class TopicClassificationAnalyzer(BaseAnalyzer):
 
             return self._get_topic_lda(source_response_list)
         elif self.method == "LDA_BERT":
-            return self.get_topics_ldabert(source_response_list)
+            return self._get_topic_ldabert(source_response_list)
+        # return LDA by default
+        return self._get_topic_lda(source_response_list)
 
-        return None
-
-    def _get_topic_bert(self, source_response_list, source_name,  umap_n_neighbors = 10, umap_n_components = 5, min_cluster_size = 10):
+    def _get_topic_bert(
+        self,
+        source_response_list: List[TextPayload],
+        source_name: str,
+        umap_n_neighbors: int = 10,
+        umap_n_components: int = 5,
+        min_cluster_size: int = 10,
+    ) -> List[TextPayload]:
         docs = [
             source_response.processed_text[: self._max_length]
             for source_response in source_response_list
         ]
         embeddings = self._get_transformer_embeddings(docs)
-        umap_embeddings = get_umap_embedings(embeddings = embeddings, n_neighbors= umap_n_neighbors, n_component= umap_n_components)
-        clusters = cluster_embeddings(umap_embeddings, min_cluster_size = min_cluster_size)
+        umap_embeddings = get_umap_embedings(
+            embeddings=embeddings,
+            n_neighbors=umap_n_neighbors,
+            n_component=umap_n_components,
+        )
+        clusters = cluster_embeddings(
+            umap_embeddings, min_cluster_size=min_cluster_size
+        )
         topics_per_cluster = get_topics_by_cluster(docs, clusters, source_name)
         return topics_per_cluster
 
-    def _get_transformer_embeddings(self, docs):
+    def _get_transformer_embeddings(self, docs: List[str]) -> List[Tensor]:
         model = SentenceTransformer(self.model_name_or_path)
         embeddings = model.encode(docs, show_progress_bar=True)
         return embeddings
@@ -81,40 +105,65 @@ class TopicClassificationAnalyzer(BaseAnalyzer):
         # if analyzer_config is None:
         #     raise ValueError("analyzer_config can't be None")
         source_name = source_response_list[0].source_name
-        analyzer_output = self._get_topics(source_response_list,source_name) # other params in config?
-        return  analyzer_output
-
-    def _get_topic_lda(self, source_input_list, source_name):
-        _, lda_model = self._get_lda_embeddings(source_input_list)
-        topics = lda_model.show_topics()
-        topics_list = [ re.findall(r'"([^"]*)"', t[1]) for t in  topics]
-        analyzer_output = [TextPayload(
-            processed_text="_".join(t),
-            meta={"cluster_topics": t},
-            source_name=source_name
-        ) for t in topics_list]
+        analyzer_output = self._get_topics(
+            source_response_list, source_name
+        )  # other params in config?
         return analyzer_output
 
-    def _get_lda_embeddings(self, source_input_list, num_topics= 10):
+    def _get_topic_lda(self, source_input_list: List[TextPayload], source_name: str):
+        _, lda_model = self._get_lda_embeddings(source_input_list)
+        topics = lda_model.show_topics()
+        topics_list = [re.findall(r'"([^"]*)"', t[1]) for t in topics]
+        analyzer_output = [
+            TextPayload(
+                processed_text="_".join(t),
+                meta={"cluster_topics": t},
+                source_name=source_name,
+            )
+            for t in topics_list
+        ]
+        return analyzer_output
+
+    def _get_lda_embeddings(
+        self, source_input_list: List[TextPayload], num_topics: int = 10
+    ):
         token_lists = self._prepare_tokens(source_input_list)
         dictionary = corpora.Dictionary(token_lists)
         corpus = [dictionary.doc2bow(text) for text in token_lists]
-        ldamodel = LdaModel(corpus, num_topics= num_topics , id2word=dictionary, passes=20)
-        embeddings = get_vec_lda(ldamodel, corpus, num_topics )
+        ldamodel = LdaModel(
+            corpus, num_topics=num_topics, id2word=dictionary, passes=20
+        )
+        embeddings = get_vec_lda(ldamodel, corpus, num_topics)
         return embeddings, ldamodel
 
-    def _prepare_tokens(self, source_response_list):
+    def _prepare_tokens(self, source_response_list: List[TextPayload]):
         tokenizer = NLTKTextTokenizer()
-        preprocess_config = TextCleanerConfig(cleaning_functions=[RemoveWhiteSpaceAndEmptyToken(),RemovePunctuation(),RemoveStopWords(),ToLowerCase()]) #Stemming
+        preprocess_config = TextCleanerConfig(
+            cleaning_functions=[
+                RemoveWhiteSpaceAndEmptyToken(),
+                RemovePunctuation(),
+                RemoveStopWords(),
+                ToLowerCase(),
+            ]
+        )  # Stemming
         preprocessor = TextCleaner()
         cleaner_responses = preprocessor.preprocess_input(
             config=preprocess_config, input_list=source_response_list
         )
-        tokenized_texts = [tokenizer.tokenize_text(t.processed_text) for t in cleaner_responses]
+        tokenized_texts = [
+            tokenizer.tokenize_text(t.processed_text) for t in cleaner_responses
+        ]
         return tokenized_texts
 
-    def _get_topic_ldabert(self, source_input_list,source_name, umap_n_neighbors = 10, umap_n_components = 5,min_cluster_size = 10  ):
-        gamma = 15 #modelparameter , importance of lda
+    def _get_topic_ldabert(
+        self,
+        source_input_list: List[TextPayload],
+        source_name: str,
+        umap_n_neighbors: int = 10,
+        umap_n_components: int = 5,
+        min_cluster_size: int = 10,
+    ):
+        gamma = 15  # modelparameter , importance of lda
         docs = [
             source_response.processed_text[: self._max_length]
             for source_response in source_input_list
@@ -125,15 +174,23 @@ class TopicClassificationAnalyzer(BaseAnalyzer):
         auto_encoder = Autoencoder()
         auto_encoder.fit(embeddings_ldabert)
         ldabert_embeddings = auto_encoder.encoder.predict(embeddings_ldabert)
-        umap_embeddings = get_umap_embedings(embeddings=ldabert_embeddings, n_neighbors=umap_n_neighbors,
-                                             n_component=umap_n_components)
-        clusters = cluster_embeddings(umap_embeddings, min_cluster_size=min_cluster_size)
+        umap_embeddings = get_umap_embedings(
+            embeddings=ldabert_embeddings,
+            n_neighbors=umap_n_neighbors,
+            n_component=umap_n_components,
+        )
+        clusters = cluster_embeddings(
+            umap_embeddings, min_cluster_size=min_cluster_size
+        )
         topics_per_cluster = get_topics_by_cluster(docs, clusters, source_name)
         return topics_per_cluster
 
 
-if __name__ == '__main__':
-    from obsei.source.playstore_scrapper import PlayStoreScrapperConfig, PlayStoreScrapperSource
+if __name__ == "__main__":
+    from obsei.source.playstore_scrapper import (
+        PlayStoreScrapperConfig,
+        PlayStoreScrapperSource,
+    )
 
     # initialize play store source config
     source_config = PlayStoreScrapperConfig(
@@ -144,16 +201,18 @@ if __name__ == '__main__':
         countries=["in"],
         package_name="com.application.zomato",
         lookup_period="1h",
-        max_count = 2000 # Lookup period from current time, format: `<number><d|h|m>` (day|hour|minute)
+        max_count=2000,  # Lookup period from current time, format: `<number><d|h|m>` (day|hour|minute)
     )
 
     # initialize play store reviews retriever
     source = PlayStoreScrapperSource()
     source_list = source.lookup(source_config)
     print("downld")
-    topic_analyzer = TopicClassificationAnalyzer(model_name_or_path = "paraphrase-TinyBERT-L6-v2 ",method = "BERT")
-    #clusters = topic_analyzer.analyze_input(source_list)
-    x = topic_analyzer._get_topic_ldabert(source_list,"ae")
+    topic_analyzer = TopicClassificationAnalyzer(
+        model_name_or_path="paraphrase-TinyBERT-L6-v2 ", method="BERT"
+    )
+    # clusters = topic_analyzer.analyze_input(source_list)
+    x = topic_analyzer._get_topic_ldabert(source_list, "ae")
     y = topic_analyzer._get_topic_lda(source_list, "pc")
     z = topic_analyzer._get_topic_bert(source_list, "cc")
 
@@ -162,5 +221,3 @@ if __name__ == '__main__':
     top_lda = [t.processed_text for t in y]
 
     a = 1
-
-
--- obsei/analyzer/topic_analyzer_utils.py
+++ obsei/analyzer/topic_analyzer_utils.py
@@ -10,27 +10,35 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 import hdbscan
 
+
 def get_umap_embedings(embeddings, n_neighbors, n_component):
-    return umap.UMAP(n_neighbors=n_neighbors,
-                            n_components=n_component,
-                            metric='cosine').fit_transform(embeddings)
+    return umap.UMAP(
+        n_neighbors=n_neighbors, n_components=n_component, metric="cosine"
+    ).fit_transform(embeddings)
+
 
 def cluster_embeddings(embeddings, min_cluster_size):
-    return hdbscan.HDBSCAN(min_cluster_size=min_cluster_size,
-                          metric='euclidean',
-                          cluster_selection_method='eom').fit(embeddings)
+    return hdbscan.HDBSCAN(
+        min_cluster_size=min_cluster_size,
+        metric="euclidean",
+        cluster_selection_method="eom",
+    ).fit(embeddings)
 
 
-def get_topics_by_cluster(docs, cluster, source_name):
+def get_topics_by_cluster(
+    docs: List[str], cluster, source_name: str
+) -> List[TextPayload]:
     docs_df = pd.DataFrame(docs, columns=["text"])
-    docs_df['topic'] = cluster.labels_
-    docs_df['doc_id'] = range(len(docs_df))
-    docs_per_topic = docs_df.groupby(['topic'], as_index = False)
-    docs_per_topic_agg = docs_per_topic.agg({'text': ' '.join})
+    docs_df["topic"] = cluster.labels_
+    docs_df["doc_id"] = range(len(docs_df))
+    docs_per_topic = docs_df.groupby(["topic"], as_index=False)
+    docs_per_topic_agg = docs_per_topic.agg({"text": " ".join})
 
     top_n_words_list = []
     tf_idf, count = c_tf_idf(docs_per_topic_agg.text.values, m=len(docs))
-    top_n_words = extract_top_n_words_per_topic(tf_idf, count, docs_per_topic_agg, n=20) #todo, can add to filter out stopwords
+    top_n_words = extract_top_n_words_per_topic(
+        tf_idf, count, docs_per_topic_agg, n=20
+    )  # todo, can add to filter out stopwords
     topic_sizes = extract_topic_sizes(docs_df)
 
     top_k = 5
@@ -39,30 +47,33 @@ def get_topics_by_cluster(docs, cluster, source_name):
         if c_no == -1:
             top_n_words_dict[-1] = "~OUTLIERS~"
         cluster = sorted(value, key=lambda x: x[1])
-        top_n_words_dict[c_no] = "_".join([i for i,j in cluster[-top_k:]])
+        top_n_words_dict[c_no] = "_".join([i for i, j in cluster[-top_k:]])
 
     for name, group in docs_per_topic:
 
         payload = TextPayload(
             processed_text=top_n_words_dict[name],
-            meta={"cluster_size": topic_sizes[name], "cluster_topics" :top_n_words_dict[name]},
+            meta={
+                "cluster_size": topic_sizes[name],
+                "cluster_topics": top_n_words_dict[name],
+            },
             segmented_data={
                 "cluster_texts": [
-                    TextPayload(
-                        processed_text = t,
-                        meta = {"cluster_id": name}
-                    )
-                for t in group["text"]]
+                    TextPayload(processed_text=t, meta={"cluster_id": name})
+                    for t in group["text"]
+                ]
             },
-            source_name = source_name
+            source_name=source_name,
         )
         top_n_words_list.append(payload)
 
+    return top_n_words_list  # return a list of textpayloads?
 
-    return top_n_words_list #return a list of textpayloads?
 
 def c_tf_idf(documents, m, ngram_range=(1, 1)):
-    count = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(documents)
+    count = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(
+        documents
+    )
     t = count.transform(documents).toarray()
     w = t.sum(axis=1)
     tf = np.divide(t.T, w)
@@ -73,24 +84,27 @@ def c_tf_idf(documents, m, ngram_range=(1, 1)):
     return tf_idf, count
 
 
-
-
 def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
     words = count.get_feature_names()
     labels = list(docs_per_topic.topic)
     tf_idf_transposed = tf_idf.T
     indices = tf_idf_transposed.argsort()[:, -n:]
-    top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+    top_n_words = {
+        label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1]
+        for i, label in enumerate(labels)
+    }
     return top_n_words
 
+
 def extract_topic_sizes(df):
-    topic_sizes = (df.groupby(['topic'])
-                     .text
-                     .count()
-                     .reset_index()
-                     .rename({"topic": "topic", "text": "Size"}, axis='columns')
-                     .sort_values("Size", ascending=False)).to_numpy()
-    return {key:value for key, value in topic_sizes}
+    topic_sizes = (
+        df.groupby(["topic"])
+        .text.count()
+        .reset_index()
+        .rename({"topic": "topic", "text": "Size"}, axis="columns")
+        .sort_values("Size", ascending=False)
+    ).to_numpy()
+    return {key: value for key, value in topic_sizes}
 
 
 def get_vec_lda(model, corpus, k):
@@ -113,7 +127,8 @@ from keras.layers import Input, Dense
 from keras.models import Model
 from sklearn.model_selection import train_test_split
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 
 class Autoencoder:
@@ -122,7 +137,7 @@ class Autoencoder:
     architecture simplified for only one hidden layer
     """
 
-    def __init__(self, latent_dim=32, activation='relu', epochs=200, batch_size=128):
+    def __init__(self, latent_dim=32, activation="relu", epochs=200, batch_size=128):
         self.latent_dim = latent_dim
         self.activation = activation
         self.epochs = epochs
@@ -144,14 +159,18 @@ class Autoencoder:
         encoded_input = Input(shape=(self.latent_dim,))
         decoder_layer = self.autoencoder.layers[-1]
         self.decoder = Model(encoded_input, self.autoencoder.layers[-1](encoded_input))
-        self.autoencoder.compile(optimizer='adam', loss=keras.losses.mean_squared_error)
+        self.autoencoder.compile(optimizer="adam", loss=keras.losses.mean_squared_error)
 
     def fit(self, X):
         if not self.autoencoder:
             self._compile(X.shape[1])
         X_train, X_test = train_test_split(X)
-        self.his = self.autoencoder.fit(X_train, X_train,
-                                        epochs=self.epochs,
-                                        batch_size=self.batch_size,
-                                        shuffle=True,
-                                        validation_data=(X_test, X_test), verbose=0)
+        self.his = self.autoencoder.fit(
+            X_train,
+            X_train,
+            epochs=self.epochs,
+            batch_size=self.batch_size,
+            shuffle=True,
+            validation_data=(X_test, X_test),
+            verbose=0,
+        )
