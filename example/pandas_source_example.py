06f6b0a4c955a44fef5a76a5dc561ea5ac99044e
Lalit Pagaria <pagaria.lalit@gmail.com>
2021-08-31 02:08:36 +0530

    Optimizing and correcting the code

--- example/pandas_source_example.py
+++ example/pandas_source_example.py
@@ -1,22 +1,28 @@
-from obsei.source.pandas import (
+from pandas import DataFrame
+
+from obsei.source.pandas_source import (
     PandasSourceConfig,
-    PandasSourceScrapper,
+    PandasSource,
 )
 import logging
 import sys
-import pandas as pd
-
-
-path = "glass1.csv"
-df = pd.read_csv(path)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+sample_dict = {
+    'name': ["aparna", "pankaj", "sudhir", "Geeku"],
+    'degree': ["MBA", "BCA", "M.Tech", "MBA"],
+    'score': [90, 40, 80, 98]
+}
+
 source_config = PandasSourceConfig(
-    dataframe=df, include_columns_list =["str"],text_columns_list = ["str"]
+    dataframe=DataFrame(sample_dict),
+    include_columns=["score"],
+    text_columns=["name", "degree"],
 )
-source = PandasSourceScrapper()
+source = PandasSource()
+
 source_response_list = source.lookup(source_config)
 for idx, source_response in enumerate(source_response_list):
     logger.info(f"source_response#'{idx}'='{source_response.__dict__}'")
---
+++ obsei/source/pandas_source.py
@@ -0,0 +1,47 @@
+from typing import List, Optional, Any
+
+from pandas import DataFrame
+
+from obsei.source.base_source import BaseSource, BaseSourceConfig
+from obsei.payload import TextPayload
+
+
+class PandasSourceConfig(BaseSourceConfig):
+    TYPE: str = "Pandas"
+
+    dataframe: DataFrame
+    text_columns: List[str]
+    separator: Optional[str] = " "
+    include_columns: Optional[List[str]] = None
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+
+        if len(self.text_columns) == 0:
+            raise ValueError('`text_columns` cannot be empty')
+
+        if not all([text_column in self.dataframe.columns for text_column in self.text_columns]):
+            raise ValueError('Every `text_columns` should be present in `dataframe`')
+
+        try:
+            self.dataframe[self.text_columns] = self.dataframe[self.text_columns].astype('string')
+        except TypeError as e:
+            raise ValueError('Unable to convert `text_columns` to string dtype')
+
+
+class PandasSource(BaseSource):
+    NAME: Optional[str] = "Pandas"
+
+    def lookup(self, config: PandasSourceConfig, **kwargs) -> List[TextPayload]:  # type: ignore[override]
+        is_include_all_cols = config.include_columns is None
+        df_to_records = config.dataframe.to_dict('records')
+        source_responses: List[TextPayload] = [
+            TextPayload(
+                processed_text=config.separator.join([record.get(text_column) for text_column in config.text_columns]),
+                meta=record if is_include_all_cols else {key: record[key] for key in config.include_columns},
+                source_name=self.NAME,
+            )
+            for record in df_to_records
+        ]
+
+        return source_responses
